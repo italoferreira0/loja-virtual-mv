51a87666c2520ffc5e614e08b70b99a5491c5128
Italo <italofsilva583@gmail.com>
2024-07-15 20:57:04 -0300

    atualização teste

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Teste</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Projeto Loja</h1>
+      <h2>Móveis Projetados</h2>
+      <p>Data de Inicio: 15/07/2024</p>
     </>
   )
 }
